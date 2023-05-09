e7d05f2226ccbaaa284679e98689fadb05a396f8
Aditya Sharma <130918775+adityadewhy@users.noreply.github.com>
2023-05-09 19:18:36 +0530

    Update NumFactorial.java
    this is an improvement but problem is its asking just once, choice should be in loop

--- NumFactorial.java
+++ NumFactorial.java
@@ -12,13 +12,24 @@ public class NumFactorial {
 
         Factorial(num);
 
+        System.out.print("do you wish to find another factorial? y/n : ");
+        String ch = input.next();
+
+        if (ch.equals("y")){
+            System.out.print("enter the new number : ");
+            num = input.nextInt();
+            Factorial(num);
+        }else{
+            System.out.print("ok, have a nice day..");
+        }
+
     }
     static void Factorial(int num){
         int temp = 1;
         for(int i = 1 ; i <= num ; i++){
             temp *= i;
         }
-        System.out.print("required factorial of " + num + " is : " + temp);
+        System.out.println("required factorial of " + num + " is : " + temp);
 
     }
 }
